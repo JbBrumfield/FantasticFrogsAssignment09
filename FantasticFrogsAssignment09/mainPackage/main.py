aac98a0096cba4d389ee4bcd8a30a31218db8898
carfornc <carfornc@LCB-10>
2025-04-02 22:55:59 -0400

    Added steps 6 and 7 to calculate number of items sold and print the sentence.

--- FantasticFrogsAssignment09/mainPackage/main.py
+++ FantasticFrogsAssignment09/mainPackage/main.py
@@ -10,7 +10,7 @@
 # Brief Description of what this module does: This module initializes the databaseManagement class and 
 # randomly selects the productID and states the manufacurer that created the product.
 # Citations: In Class work, https://stackoverflow.com/questions/28981770/store-sql-result-in-a-variable-in-python,
- 
+ #https://stackoverflow.com/questions/902408/how-to-use-variables-in-sql-statement-in-python
 # Anything else that's relevant:
 
 import pyodbc
@@ -40,22 +40,40 @@ def main():
     manufacturer_id = selected_row[3]
     brand_id = selected_row[4]
 
-    print(f"Randomly selected product: {description} (ProductID: {product_id})")
+    # print(f"Randomly selected product: {description} (ProductID: {product_id})")
 
    
     manufacturer_name = dbm.fetch_manufacturer_name(manufacturer_id, conn)
-
+    """
     if manufacturer_name:
         print(f"The manufacturer of this product is: {manufacturer_name}")
     else:
         print(f"Manufacturer with ID {manufacturer_id} not found.") 
-
+    """
     brand_name = dbm.fetch_brand_name(brand_id, conn)
  
+    """
     if brand_name:
         print(f"The brand of this product is: {brand_name}")
     else:
         print(f"Brand with ID {brand_id} not found.")
+    """
+    sql_sales_query = f"""
+        SELECT TOP (100) PERCENT SUM(dbo.tTransactionDetail.QtyOfProduct) AS NumberOfItemsSold
+        FROM dbo.tTransactionDetail INNER JOIN dbo.tTransaction ON dbo.tTransactionDetail.TransactionID = dbo.tTransaction.TransactionID
+        WHERE (dbo.tTransaction.TransactionTypeID = 1) AND (dbo.tTransactionDetail.ProductID = {product_id})
+        """
+
+    NumberOfItemsSold = dbm.submit_sql_to_server(sql_sales_query, conn)
+
+    if NumberOfItemsSold and   NumberOfItemsSold[0][0] is not None:
+            number_of_items_sold = NumberOfItemsSold[0][0]
+    else:
+            number_of_items_sold = 0
+    #print(NumberOfItemsSold)
+    sentence = f"The {description}, manufactured by {manufacturer_name} under the brand {brand_name}, has sold {number_of_items_sold} items."
+    print(sentence)
+
 
 if __name__ == "__main__":
     main()
