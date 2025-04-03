87f9297cbeab42fab3405b81887d4880421004a3
Jacob Brumfield <brumfijb@mail.uc.edu>
2025-04-02 21:49:20 -0400

    Add project files.

---
+++ FantasticFrogsAssignment09/dataPackage/databaseManagement.py
@@ -0,0 +1,15 @@
+# File Name : databaseManagement.py
+# Student Name: Jacob Brumfield, Nikki Carfora, Ray Happel
+# email:  brumfijb@mail.uc.edu, happelrc@mail.uc.edu
+# Assignment Number: Assignment 09
+# Due Date:  4/3/2025
+# Course #/Section:  IS 4010 001
+# Semester/Year:   Spring 2025
+# Brief Description of the assignment:  Connect to the professors SQL database to produce interesting results
+ 
+# Brief Description of what this module does: This module connects to our SQL database GroceryStoreSimulator and querys the database for different information.
+# Citations: In Class work, https://stackoverflow.com/questions/28981770/store-sql-result-in-a-variable-in-python,
+ 
+# Anything else that's relevant:
+
+
---
+++ FantasticFrogsAssignment09/mainPackage/main.py
@@ -0,0 +1,16 @@
+# File Name : main.py
+# Student Name: Jacob Brumfield, Nikki Carfora, Ray Happel
+# email:  brumfijb@mail.uc.edu, happelrc@mail.uc.edu
+# Assignment Number: Assignment 09
+# Due Date:  4/3/2025
+# Course #/Section:  IS 4010 001
+# Semester/Year:   Spring 2025
+# Brief Description of the assignment:  Connect to the professors SQL database to produce interesting results
+ 
+# Brief Description of what this module does: This module initializes the databaseManagement class and 
+# randomly selects the productID and states the manufacurer that created the product.
+# Citations: In Class work, https://stackoverflow.com/questions/28981770/store-sql-result-in-a-variable-in-python,
+ 
+# Anything else that's relevant:
+
+
